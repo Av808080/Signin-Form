f2ab845e73a446bc1ef95467dac18f78ffdda344
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-04-08 18:25:40 +0330

    Update Movie.js

--- pages/Movie.js
+++ pages/Movie.js
@@ -9,9 +9,7 @@ const Movie = () => {
       .get(`${URL}${input.current.value}`)
       .then((response) => setdata(response.data));
   };
-//   console.log(data);
-  //   if(data)
-  //   return<div>NO MOVIE WITH THIS NAME</div>
+  
   return (
     <div>
       <input type="text" ref={input} className="border-slate-700 border" />
